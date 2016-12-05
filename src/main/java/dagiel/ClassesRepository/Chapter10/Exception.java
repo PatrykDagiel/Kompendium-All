0e6da67a0dc226032875926bca33779086a08a6c
Patryk Dagiel <patryk.dagiel@gmail.com>
2016-12-05 17:04:14 +0100

    Commit 5/12

---
+++ src/main/java/dagiel/ClassesRepository/Chapter10/Exception.java
@@ -0,0 +1,19 @@
+package dagiel.ClassesRepository.Chapter10;
+
+/**
+ * Created by dagiel on 2016-12-05.
+ */
+public class Exception {
+    public static void main(String[] args) {
+        int d, a;
+
+        try {
+            d = 0;
+            a = 42 / d;
+            System.out.println("Nie zostanie wyswietlone");
+        } catch (ArithmeticException e) {
+            System.out.println("Dzielenie przez zero");
+        }
+        System.out.println("Po bloku try-catch");
+    }
+}
---
+++ src/main/java/dagiel/ClassesRepository/Chapter10/HandlerError.java
@@ -0,0 +1,17 @@
+package dagiel.ClassesRepository.Chapter10;
+
+import java.util.Random;
+
+/**
+ * Created by dagiel on 2016-12-05.
+ */
+public class HandlerError {
+    public static void main(String[] args) {
+        int a = 0, b = 0, c = 0;
+        Random r = new Random();
+
+        for (int i = 0; i < 32000; i++) {
+            try
+        }
+    }
+}
