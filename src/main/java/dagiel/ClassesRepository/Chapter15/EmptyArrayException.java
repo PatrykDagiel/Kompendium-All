fc7a95916e020c324f81e4674eaab464a60c41ff
Patryk Dagiel <patryk.dagiel@gmail.com>
2017-03-14 16:26:09 +0100

    Commit 14/03/2017 ch.II

---
+++ src/main/java/dagiel/ClassesRepository/Chapter15/EmptyArrayException.java
@@ -0,0 +1,10 @@
+package dagiel.ClassesRepository.Chapter15;
+
+/**
+ * Created by dagiel on 14/03/2017.
+ */
+public class EmptyArrayException extends Exception {
+    EmptyArrayException() {
+        super("Array Empty");
+    }
+}
